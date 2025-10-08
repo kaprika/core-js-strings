cfed2ea8b9dcecca5bcb3fe8c631d2023095bc93
Elena Petrevich <elysiuk@gmail.com>
2025-10-08 10:25:13 +0300

    warnings fixed

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-restricted-syntax */
 /* *******************************************************************************************
  *                                                                                           *
  * Please read the following tutorial before implementing tasks:                              *
